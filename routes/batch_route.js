812dc4aa706bf146211afec8218bb71bef0b9580
kunal mittal <kanumittal99@gmail.com>
2018-06-01 13:35:58 +0530

    changes in college route

--- routes/batch_route.js
+++ routes/batch_route.js
@@ -1,13 +1,9 @@
 const app=require('../admin.js');
 const control=require('../controllers/batch_control.js');
 module.exports=(app)=>{
+    app.get('/batch/get_college/all',control.getAllCollege);
     app.get('/batch/:course',control.getStudents);
     app.post('/batch/create_batch',control.createBatch);
-<<<<<<< HEAD
-    app.post('/batch/search_bacth',control.searchBatch);
-    app.get('/batch/get_college/all',control.getAllCollege);
-=======
     app.post('/batch/search_batch',control.searchBatch);
     app.post('/batch/search_one_batch',control.searchOneBatch);
->>>>>>> cf0bd104b3d069190cd1a0ab4cfd17630b50ebb8
 }
